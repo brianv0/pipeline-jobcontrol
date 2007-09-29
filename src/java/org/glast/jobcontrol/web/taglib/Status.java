3cd17bf58f6852d420f66952c71fadd95c185f2d
Tony Johnson <tony-johnson@users.noreply.github.com>
2007-09-29 19:09:08 +0000

    Add ability to specify host etc from tld functions

--- src/java/org/glast/jobcontrol/web/taglib/Status.java
+++ src/java/org/glast/jobcontrol/web/taglib/Status.java
@@ -9,19 +9,23 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 /**
  *
  * @author tonyj
- * @version $Id: Status.java,v 1.2 2007-05-14 20:43:17 tonyj Exp $
+ * @version $Id: Status.java,v 1.3 2007-09-29 19:09:08 tonyj Exp $
  */
 public class Status extends SimpleTagSupport
 {
    private String id;
    private String var;
+   private String serviceName;
+   private String host;
+   private String user;
+   private int port = 1099;
    
    public void doTag() throws JspException
    {  
       JobStatus status;
       try
       {
-         JobControlClient client = new JobControlClient();
+         JobControlClient client = new JobControlClient(user, host, port, serviceName);
          status = client.status(id);
       }
       catch (NoSuchJobException ex)
@@ -42,5 +46,21 @@ public class Status extends SimpleTagSupport
    public void setId(String id)
    {
       this.id = id;
+   }
+   public void setHost(String host)
+   {
+      this.host = host;
+   }
+   public void setUser(String user)
+   {
+      this.user = user;
+   }
+   public void setServiceName(String serviceName)
+   {
+      this.serviceName = serviceName;
+   }
+   public void setPort(int port)
+   {
+      this.port = port;
    }
 }
