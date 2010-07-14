d7d205b783678d8c5c9ab981655137b8bd68f180
Tony Johnson <tony-johnson@users.noreply.github.com>
2010-07-14 22:58:35 +0000

    File for testing condor job submission daemon

---
+++ src/java/org/glast/jobcontrol/demo/CondorJobControlTest.java
@@ -0,0 +1,41 @@
+package org.glast.jobcontrol.demo;
+
+import org.glast.jobcontrol.Job;
+import org.glast.jobcontrol.JobControlClient;
+import org.glast.jobcontrol.JobControlException;
+import org.glast.jobcontrol.JobSubmissionException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tony Johnson
+ */
+public class CondorJobControlTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws JobSubmissionException, JobControlException {
+        if (args.length < 1) {
+            usage();
+        }
+        String command = args[0];
+        List<String> arguments = new ArrayList<String>();
+        for (int i = 2; i < args.length; i++) {
+            arguments.add(args[i]);
+        }
+
+        Job job = new Job();
+        job.setCommand(command);
+        job.setArguments(arguments);
+        JobControlClient client = new JobControlClient("tjohnson", "smuhpc.smu.edu", 1099, "JobControlService");
+        String id = client.submit(job);
+        System.out.println("Job " + id + " submitted");
+    }
+
+    private static void usage() {
+        System.out.println("usage: java " + CondorJobControlTest.class.getName() + "command [args...]");
+        System.exit(0);
+    }
+}
