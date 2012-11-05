f5ced193376b7e27005ad71a4a23579aa3eb80aa
zimmer <zimmer@106b4de9-e96a-4e09-a5f9-d065fb1fdc55>
2012-11-05 23:17:07 +0000

    implement first version of DIRAC Job Control Service. Utilizes DIRAC API using python code stored in src/resource/python; status query through xml/JAXB, json used for casting pipeline environment to DIRAC code

--- org-srs-jobcontrol/trunk/src/java/org/srs/jobcontrol/DIRAC/DIRACJobControlService.java
+++ org-srs-jobcontrol/trunk/src/java/org/srs/jobcontrol/DIRAC/DIRACJobControlService.java
@@ -1,9 +1,8 @@
-/*
+/*  
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package org.srs.jobcontrol.DIRAC;
-import java.io.File;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.rmi.RemoteException;
@@ -18,12 +17,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.management.JMException;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
+import org.json.JSONObject;
 import org.srs.jobcontrol.Job;
 import org.srs.jobcontrol.JobControl;
 import org.srs.jobcontrol.JobControlException;
@@ -32,15 +31,14 @@ import org.srs.jobcontrol.JobSubmissionException;
 import org.srs.jobcontrol.NoSuchJobException;
 import org.srs.jobcontrol.OutputProcessor;
 import org.srs.jobcontrol.common.JobControlService;
-import org.srs.jobcontrol.common.JobControlService.DeleteFile;
-
 /**
  *
  * @author zimmer
+ * TODO: handle creation of archive folders
  */
 public class DIRACJobControlService extends JobControlService{
     private final static String SUBMIT_COMMAND = System.getProperty("org.srs.jobcontrol.DIRACsubmitCommand","dirac-submit");
-    private final static String KILL_COMMAND = System.getProperty("org.srs.jobcontrol.DIRACkillCommand","dirac-delete");
+    private final static String KILL_COMMAND = System.getProperty("org.srs.jobcontrol.DIRACkillCommand","dirac-wms-job-kill");
     private final static Pattern pattern = Pattern.compile("Your job (\\w+)*");
     private final DIRACStatus DIRACStatus = new DIRACStatus();    
     
@@ -54,7 +52,7 @@ public class DIRACJobControlService extends JobControlService{
         // Bind the remote object's stub in the registry
         Registry registry = LocateRegistry.getRegistry();
         registry.rebind("DIRACJobControlService-"+user, stub);
-        service.logger.info("Server ready, user "+user);
+        DIRACJobControlService.logger.info("Server ready, user "+user);
         
         // Register the JMX bean
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
@@ -67,11 +65,11 @@ public class DIRACJobControlService extends JobControlService{
 	
 	    logger.info("BEGIN DIRAC JobControlService submit");
             String ip = RemoteServer.getClientHost();
-            logger.info("submitting: "+job.getCommand()+" from "+ip);
+            logger.log(Level.INFO, "submitting: {0} from {1}", new Object[]{job.getCommand(), ip});
             checkPermission(ip);
             
             String jobName = submitInternal(job);
-            logger.fine("job "+jobName+" submitted");
+            logger.log(Level.FINE, "job {0} submitted", jobName);
 	    nSubmitted.incrementAndGet();
             lastSuccessfulJobSubmissionTime = System.currentTimeMillis();
 	    logger.info("END DIRAC JobControlService submit");
@@ -93,51 +91,36 @@ public class DIRACJobControlService extends JobControlService{
         }
     }
     private String submitInternal(Job job) throws JobSubmissionException, JobControlException {
+        //TODO: implement env as dict object in file (serialized)
         //logger.info("BEGIN submitInternal");
         String command = job.getCommand();
-	logger.info("command:" + command);
+	logger.log(Level.INFO, "command:{0}", command);
 	
-        if (command == null || command.length() == 0) throw new JobSubmissionException("Missing command");
+        if (command == null || command.length() == 0) {
+            throw new JobSubmissionException("Missing command");
+        }
         List<String> qsub = new ArrayList<String>(Arrays.<String>asList(SUBMIT_COMMAND.split("\\s+")));
-        String logFileName = job.getLogFile()==null ? "logFile.log" : sanitize(job.getLogFile());
-        qsub.add("-out=");
-        qsub.add(job.getWorkingDirectory()+"/"+logFileName);// SZ 2011-07-18, GE needs path for logFile
+        
+        qsub.add(command);
+        qsub.add("-p");
         if (job.getMaxCPU() != 0) 
         { 
-           qsub.add("-cpu="+convertToNormalisedSec(job.getMaxCPU()));
+           qsub.add("cpu="+convertToNormalisedSec(job.getMaxCPU()));
         }
-        if (job.getMaxMemory() != 0) 
-        { 
-           qsub.add("-mem="+job.getMaxMemory()+"M");
-        }
+        // the later created json file containing the pipeline env vars is referenced here
+        qsub.add("-p");
+        qsub.add("env=pipeline_environment.json"); 
         
-        // scratch space is defined in extra options!
+        // everything else is defined in extra options
         if (job.getExtraOptions() != null)
         { 
             qsub.addAll(tokenizeExtraOption(job.getExtraOptions())); 
         }
         // DIRAC only accepts a script as a argument, not a command
         // Also DIRAC does not automatically copy the current working directory to the job
-        String DIRAC_Command = "dirac_script";
-        if (job.getWorkingDirectory() != null)
-        {
-           DIRAC_Command = job.getWorkingDirectory()+"/"+DIRAC_Command;
-        }
-        qsub.add(DIRAC_Command);
         
-        StringBuilder dirac_script = new StringBuilder();
-        if (job.getWorkingDirectory() != null)
-        {
-           dirac_script.append("cd ").append(job.getWorkingDirectory()).append('\n');
-        }
-        dirac_script.append(command);
-        if (job.getArguments() != null) {
-            for (String arg : job.getArguments()) { dirac_script.append(" \""+arg+"\""); }
-        }
-        dirac_script.append('\n');
-        job.getFiles().put("dirac_script",dirac_script.toString());
         String fullCommand = toFullCommand(qsub);
-        logger.info("Submit: "+fullCommand);
+        logger.log(Level.INFO, "Submit: {0}", fullCommand);
              
         // Things to be undone if the submit fails.
         List<Runnable> undoList = new ArrayList<Runnable>();
@@ -150,43 +133,9 @@ public class DIRACJobControlService extends JobControlService{
             if (job.getEnv() != null) {
                 env.putAll(job.getEnv());
             }
-	    
-            if (job.getWorkingDirectory() != null)
-            {
-                env.put("JOBCONTROL_WORKINGDIR",job.getWorkingDirectory());
-                File dir = new File(job.getWorkingDirectory());
-               for (int retry : retryDelays)
-               {
-                  if (!dir.exists())
-                  {
-                     // This occasionally fails due to NFS/automount problems, so retry a few times
-                     boolean rc = dir.mkdirs();
-                     if (!rc) 
-                     {
-                        if (retry > 0)
-                        {
-                           Thread.sleep(retry);
-                           continue;
-                        }
-                        else throw new JobSubmissionException("Could not create working directory "+dir);
-                     }
-                     else 
-                     {
-                        undoList.add(new DeleteFile(dir));
-                     }
-                  }
-                  else if (!dir.isDirectory()) throw new JobSubmissionException("Working directory is not a directory "+dir);
-                  else if (job.getArchiveOldWorkingDir() != null)
-                  {
-                     archiveOldWorkingDir(dir, job.getArchiveOldWorkingDir(),undoList);
-                  }
-                  break;
-               }
-               builder.directory(dir);
-               env.put("JOBCONTROL_LOGFILE",new File(dir,logFileName).getAbsolutePath());
-               storeFiles(dir, job.getFiles(), undoList);
-            }
-	    
+            JSONObject env_pipeline_json = new JSONObject(env);
+            job.getFiles().put("pipeline_environment.json", env_pipeline_json.toString()); // that should create this file in the working directory?
+            
             builder.redirectErrorStream(true);
             Process process = builder.start();
             OutputProcessor output = new OutputProcessor(process.getInputStream(),logger);
@@ -196,10 +145,12 @@ public class DIRACJobControlService extends JobControlService{
             
             List<String> result = output.getResult();
             int rc = process.exitValue();
-            logger.info("process.exitValue=" + rc);
+            logger.log(Level.INFO, "process.exitValue={0}", rc);
             if (rc != 0) {
                StringBuilder message = new StringBuilder("Process failed rc="+rc);
-               if (!result.isEmpty()) message.append(" output was:");
+               if (!result.isEmpty()) {
+                    message.append(" output was:");
+                }
                for (String line : result)
                {
                   message.append('\n').append(line);
@@ -207,12 +158,16 @@ public class DIRACJobControlService extends JobControlService{
                throw new JobControlException(message.toString());
             }
             
-            if (output.getStatus() != null) throw output.getStatus();
+            if (output.getStatus() != null) {
+                throw output.getStatus();
+            }
 
-            if (result.size() == 0) throw new JobControlException("Unexpected output length "+result.size());
+            if (result.isEmpty()) {
+                throw new JobControlException("Unexpected output length "+result.size());
+            }
             for (String line : result) {
 	    
-	        logger.info("line:" + line);
+	        logger.log(Level.INFO, "line:{0}", line);
                 Matcher matcher = pattern.matcher(line);
 		  
                 boolean ok = matcher.find();
@@ -231,7 +186,9 @@ public class DIRACJobControlService extends JobControlService{
         }
         finally {
             Collections.reverse(undoList);
-            for (Runnable undo : undoList) undo.run();
+            for (Runnable undo : undoList) {
+                undo.run();
+            }
         }
     }
 
@@ -241,9 +198,12 @@ public class DIRACJobControlService extends JobControlService{
     }
     private void checkPermission(String ip) throws SecurityException {
         //if (!ip.startsWith("134.79") && !ip.startsWith("198.129")) throw new SecurityException();
-        if (!ip.startsWith("134.158") && !ip.startsWith("134.79")) throw new SecurityException();
-	else
-	  System.out.println("Permission OK");
+        if (!ip.startsWith("134.158") && !ip.startsWith("134.79")) {
+            throw new SecurityException();
+        }
+	else {
+            System.out.println("Permission OK");
+        }
     }
     public JobStatus status(String jobID) throws NoSuchJobException, JobControlException {
         try {
@@ -253,9 +213,12 @@ public class DIRACJobControlService extends JobControlService{
             System.out.println("status: "+jobID+" from "+ip);
             checkPermission(ip);
             
-            Map<String,JobStatus> statii = DIRACStatus.getStatus();
+            Map<String,JobStatus> statii;
+            statii = DIRACStatus.getStatus();
             JobStatus result = statii.get(jobID);
-            if (result == null) throw new NoSuchJobException("Job id "+jobID);
+            if (result == null) {
+                throw new NoSuchJobException("Job id "+jobID);
+            }
             return result;
         } catch (ServerNotActiveException t) {
             logger.log(Level.SEVERE,"Unexpected error",t);
@@ -272,11 +235,11 @@ public class DIRACJobControlService extends JobControlService{
     public void cancel(String jobID) throws NoSuchJobException, JobControlException {
         try {
             String ip = RemoteServer.getClientHost();
-            logger.info("killing: "+jobID+" from "+ip);
+            logger.log(Level.INFO, "killing: {0} from {1}", new Object[]{jobID, ip});
             checkPermission(ip);
             
             cancelInternal(jobID);
-            logger.fine("job "+jobID+" cancelled");
+            logger.log(Level.FINE, "job {0} cancelled", jobID);
         } catch (ServerNotActiveException t) {
             logger.log(Level.SEVERE,"Unexpected error",t);
             throw new JobControlException("Unexpected error",t);
@@ -301,8 +264,12 @@ public class DIRACJobControlService extends JobControlService{
             process.waitFor();
             output.join();
             int rc = process.exitValue();
-            if (rc == 255) throw new NoSuchJobException("No such job, id="+jobID);
-            else if (rc != 0) throw new JobControlException("Command failed rc="+rc);
+            if (rc == 255) {
+                throw new NoSuchJobException("No such job, id="+jobID);
+            }
+            else if (rc != 0) {
+                throw new JobControlException("Command failed rc="+rc);
+            }
         } catch (IOException x) {
             throw new JobControlException("IOException while killing job "+jobID,x);
         } catch (InterruptedException x) {
@@ -338,3 +305,4 @@ public class DIRACJobControlService extends JobControlService{
    }
     
 }
+//
--- org-srs-jobcontrol/trunk/src/java/org/srs/jobcontrol/DIRAC/DIRACStatus.java
+++ org-srs-jobcontrol/trunk/src/java/org/srs/jobcontrol/DIRAC/DIRACStatus.java
@@ -9,34 +9,26 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import org.srs.jobcontrol.*;
 import org.srs.jobcontrol.common.CommonJobStatus;
-
-/**
+import org.srs.jobcontrol.DIRAC.StatusXML.*;
+/*
  *
  * @author zimmer
  */
-public class DIRACStatus {
+class DIRACStatus
+{
    private final static long CACHE_TIME = 60*1000; // Needed to avoid excessive calls to bjobs
-   private final static String STATUS_COMMAND = "dirac-status"; // change to new mapping
-   //jobname   uname   status     worker            worker             qtime               stime               etime               cputime     cur_mem     cur_scratch
-   //I3432967 clavalle ENDED      ccwl0494.in2p3.fr ccwl0494.in2p3.fr  09/26/2006-15:49:04 09/26/2006-15:53:10 09/26/2006-19:04:56 10300       274         0
-   // (\\d+)\\s+(\\S+)\\s+(\\S+)  \\s+(\\S+)        \\s+(\\S+)         \\s+(\\S+)          \\s+(\\S+)          \\s+(\\S+)           \\s+(\\d+) \\s+(\\d+)  \\s+(\\d+)
-   //INFO: NoMatch: "I6462026 clavalle QUEUED - - 02/27/2007-15:27:51 - - - - -"
-	    
-   private final static Pattern pattern = Pattern.compile("(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\d+)\\s+(\\d+)\\s+(\\d+)");
-   private final static Pattern patternQueued = Pattern.compile("(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\W)\\s+(\\W)\\s+(\\S+)\\s+(\\W)\\s+(\\W)\\s+(\\W)\\s+(\\W)\\s+(\\W)");
-
-   private final static Pattern timePattern = Pattern.compile("(\\d+):(\\d+):(\\d+).(\\d+)");
-   private final static SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy-HH:mm:ss");
+   private final static String STATUS_COMMAND = System.getProperty("org.srs.jobcontrol.DIRACstatusCommand","dirac-status -p xml=True");
    private final static Logger logger = Logger.getLogger("org.srs.jobcontrol.DIRACStatus");
    
    private Map<String,JobStatus> map;
@@ -54,155 +46,71 @@ public class DIRACStatus {
          String command = STATUS_COMMAND;
          List<String> commands = new ArrayList<String>(Arrays.asList(command.split("\\s+")));
          ProcessBuilder builder = new ProcessBuilder();
-         builder.redirectErrorStream(true);
+         
          builder.command(commands);
          Process process = builder.start();
-         OutputProcessor output = new OutputProcessor(process.getInputStream(),logger);
+         OutputProcessor output = new OutputProcessor(process.getErrorStream(),logger);
          process.waitFor();
          output.join();
          int rc = process.exitValue();
-         if (rc != 0) throw new JobControlException("Process failed, rc="+rc);
+         if (rc != 0) {
+              throw new JobControlException("Process failed, rc="+rc);
+          }
          
-         if (output.getStatus() != null) throw output.getStatus();
+         JAXBContext jc = JAXBContext.newInstance ("org.srs.jobcontrol.DIRAC.StatusXML");
+         Unmarshaller um = jc.createUnmarshaller ();
+         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
          
-         List<String> result = output.getResult();
-         if (result.size() == 0) throw new JobControlException("Unexpected output length "+result.size());
-         logger.info("Status returned "+result.size()+" lines");
-	 
-         
-	 
-         Map<String,JobStatus> map = new HashMap<String,JobStatus>();
-         
-         for (int i=0; i<result.size(); i++)
-         {
-	    logger.info("result line # " + i +": " + result.get(i));
-
-            //Matcher matchNormal = pattern.matcher(result.get(i));
-            Matcher matchQueued = patternQueued.matcher(result.get(i));
-	    //Matcher match=patternQueued.matcher(result.get(i));
-	    //boolean matchFound=false;
-	    //boolean matchQueuedFound=false;
-	    
-	    /**if(matchNormal.matches()){
-	      logger.info("Match normal pattern: \""+result.get(i)+"\"");
-	      match = pattern.matcher(result.get(i));
-	      matchFound = true;
-	    }
-	    else{ 
-	      logger.info("NoMatch normal pattern: \""+result.get(i)+"\"");
-	      if (matchQueued.matches()){
-	        logger.info("Match queued pattern");
-		match = patternQueued.matcher(result.get(i));
-		logger.info("id=" + match.group(1));
-		matchQueuedFound = true;
-	      }
-	      else 
-	        logger.info("No Match either normal nor queued pattern");
-	    }
-	   
-            if (matchFound || matchQueuedFound)
-            {*/
-
-	    Matcher match = pattern.matcher(result.get(i));
-	    if(match.matches()){   
-               try
-               {
-	         // logger.info("matchFound, matchQueuedFound:" + matchFound + "," + matchQueuedFound);
-                  CommonJobStatus stat = new CommonJobStatus();
-                  String id = match.group(1);
-		  logger.info("id=" + id);
-                  stat.setId(id);
-                  stat.setUser(match.group(2));
-                  stat.setStatus(toStatus(match.group(3)));
-		  
-		  
-                    stat.setQueue(match.group(4));
-                    stat.setHost(match.group(5));
-                    stat.setSubmitted(toDate(match.group(6)));
-                    stat.setStarted(toDate(match.group(7)));               
-                    stat.setEnded(toDate(match.group(8)));  
-                    //stat.setCpuUsed(toTime(match.group(9)));
-                    stat.setCpuUsed(Integer.parseInt(match.group(9))); // time in seconds 
-                    stat.setMemoryUsed(Integer.parseInt(match.group(10)));
-                    stat.setSwapUsed(Integer.parseInt(match.group(11)));
-                  
-		  
-
-                  StringBuffer comment = null;
-                  while ((i+1)<result.size() && result.get(i+1).charAt(0) == ' ')
-                  {
-                     i++;
-                     if (comment == null) comment = new StringBuffer();
-                     else comment.append('\n');
-                     comment.append(result.get(i).substring(1));
-                  }
-                  if (comment != null) stat.setComment(comment.toString());
-                  
-                  map.put(id,stat);
-               }
-               catch (ParseException x)
-               {
-                  x.printStackTrace();
-               }
-            } 
-	    else{ 
-	      logger.info("NoMatch normal pattern: \""+result.get(i)+"\"");
-	      if (matchQueued.matches()){
-	        logger.info("Match queued pattern, number of groups=" + matchQueued.groupCount());
-		logger.info("group1=" + matchQueued.group(1));
-		
-		try
-        	{
-	          // logger.info("matchFound, matchQueuedFound:" + matchFound + "," + matchQueuedFound);
-                   CommonJobStatus stat = new CommonJobStatus();
-                   String id = matchQueued.group(1);
-		   logger.info("id=" + id);
-                   stat.setId(id);
-                   stat.setUser(matchQueued.group(2));
-                   stat.setStatus(toStatus(matchQueued.group(3)));
-
-
-                     //stat.setQueue(matchQueued.group(4));
-                     //stat.setHost(matchQueued.group(5));
-                     stat.setSubmitted(toDate(matchQueued.group(6)));
-                     //stat.setStarted(toDate(matchQueued.group(7)));               
-                     //stat.setEnded(toDate(matchQueued.group(8)));  
-                     //stat.setCpuUsed(toTime(match.group(9)));
-                     //stat.setCpuUsed(Integer.parseInt(match.group(9))); // time in seconds 
-                     //stat.setMemoryUsed(Integer.parseInt(match.group(10)));
-                     //stat.setSwapUsed(Integer.parseInt(match.group(11)));
-
-
-
-                   StringBuffer comment = null;
-                   while ((i+1)<result.size() && result.get(i+1).charAt(0) == ' ')
-                   {
-                      i++;
-                      if (comment == null) comment = new StringBuffer();
-                      else comment.append('\n');
-                      comment.append(result.get(i).substring(1));
-                   }
-                   if (comment != null) stat.setComment(comment.toString());
-
-                   map.put(id,stat);
-        	}
-        	catch (ParseException x)
-        	{
-                   x.printStackTrace();
-        	}
-
-		
-	      }
-	      else 
-	        logger.info("No Match either normal nor queued pattern");
-	    }     
+         final JAXBElement element = (JAXBElement)um.unmarshal (process.getInputStream());
+         final Joblist joblist = (Joblist)element.getValue();
+         // main loop over all jobs
+         for (Joblist.Job job : joblist.getJob()){
+             final String job_id = String.valueOf(job.getJobID());
+             CommonJobStatus stat = new CommonJobStatus();
+             stat.setId(job_id);
+             if (job.getTotalCPUTimes()!=null){
+                 stat.setCpuUsed(Math.round(job.getTotalCPUTimes()));
+             }
+             if (job.getMemorykB()!=null){
+                 String memory_kb = job.getMemorykB().replace("kB",""); // now we removed the kB
+                 Float memory_kb_flt = Float.valueOf(memory_kb).floatValue();
+                 int memory_used = Math.round(memory_kb_flt)*1024;
+                 stat.setMemoryUsed(memory_used);
+             }
+             if (job.getSite()!=null){
+                 stat.setHost(job.getSite());
+             }
+             
+             if (job.getStarted()!=null){
+                 final String str_started_time = job.getStarted();
+                 Date start_time = (Date)formatter.parse(str_started_time);
+                 stat.setStarted(start_time);
+             }
+             if (job.getEnded()!=null){
+                 final String str_end_time = job.getEnded();
+                 Date end_time = (Date)formatter.parse(str_end_time);
+                 stat.setStarted(end_time);
+             }
+             if (job.getSubmitted()!=null){
+                 final String str_sub_time = job.getEnded();
+                 Date submit_time = (Date)formatter.parse(str_sub_time);
+                 stat.setStarted(submit_time);
+             }
+             if (job.getPilotReference()!=null){
+                 final String pilot_reference = job.getPilotReference();
+                 stat.setComment("pilot reference="+pilot_reference);
+             }
+            map.put(job_id,stat); 
          }
-         synchronized (this)
+         System.out.println("*INFO* statuses found=" + map);
+   
+	 synchronized (this)
          {
             this.map = map;
             this.timeStamp = System.currentTimeMillis();
          }
-      }
+         }
       catch (IOException x)
       {
          throw new JobControlException("IOException during job submission",x);
@@ -211,41 +119,47 @@ public class DIRACStatus {
       {
          throw new JobControlException("Job submission interrupted",x);
       }
-   }
-   private Date toDate(String date) throws ParseException
-   {
-      if (date.equals("-")) return null;
-      Calendar now = Calendar.getInstance();
-      int year = now.get(Calendar.YEAR);
-      // FIXME: What about new year?
-      //Date d = dateFormat.parse(year+"/"+date);
-      Date d = dateFormat.parse(date);
-      return d;
-   }
-   private int toTime(String date) throws ParseException
-   {
-      Matcher match = timePattern.matcher(date);
-      if (match.matches())
+      catch (JAXBException e) 
       {
-         int hour = Integer.parseInt(match.group(1));
-         int minute = Integer.parseInt(match.group(2));
-         int second = Integer.parseInt(match.group(3));
-         return (hour*60+minute)*60+second;
+         throw new JobControlException("caught JAXB Exception",e);
       }
-      else return 0;
+      catch (ParseException e){
+          throw new JobControlException("caught Exception parsing date",e);
+      }
+          
    }
    private JobStatus.Status toStatus(String status)
    {
       System.out.println("status found=" + status);
-      if      ("QUEUED".equals(status)) return JobStatus.Status.PENDING;
-      else if ("HOLD".equals(status))  return JobStatus.Status.WAITING;
-      else if ("SUBMITTED".equals(status)) return JobStatus.Status.PENDING;
-      else if ("STARTED".equals(status)) return JobStatus.Status.RUNNING;
-      else if ("RUNNING".equals(status))  return JobStatus.Status.RUNNING;
-      else if ("ENDED".equals(status)) return JobStatus.Status.DONE;
-      else if ("DELETED".contains(status)) return JobStatus.Status.FAILED;
-      else if ("KILLED".contains(status)) return JobStatus.Status.FAILED;
-      else return JobStatus.Status.UNKNOWN;
+      // statii queried by dirac-status command
+      //'Done','Completed','Stalled','Failed','Killed','Waiting','Running','Checking'
+      if      ("Done".equals(status)) {
+           return JobStatus.Status.DONE;
+       }
+      else if ("Completed".equals(status)) {
+           return JobStatus.Status.DONE;
+       }
+      else if ("Stalled".equals(status)) {
+           return JobStatus.Status.SUSPENDED;
+       }
+      else if ("Failed".equals(status)) {
+           return JobStatus.Status.FAILED;
+       }
+      else if ("Killed".equals(status)) {
+           return JobStatus.Status.FAILED;
+       }
+      else if ("Waiting".equals(status)) {
+           return JobStatus.Status.WAITING;
+       }
+      else if ("Running".equals(status)) {
+           return JobStatus.Status.RUNNING;
+       }
+      else if ("Checking".equals(status)) {
+           return JobStatus.Status.PENDING;
+       }
+      else {
+           return JobStatus.Status.UNKNOWN;
+       }
    }
    Map<String, JobStatus> getStatus() throws JobControlException
    {
@@ -253,10 +167,11 @@ public class DIRACStatus {
       {
          long now = System.currentTimeMillis();
          boolean updateNeeded = now-timeStamp > CACHE_TIME;
-         logger.fine("status: now="+now+" timeStamp="+timeStamp+" cache="+CACHE_TIME+" update needed: "+updateNeeded);
-         if (updateNeeded) updateStatus();
+         logger.log(Level.FINE, "status: now={0} timeStamp={1} cache={2} update needed: {3}", new Object[]{now, timeStamp, CACHE_TIME, updateNeeded});
+         if (updateNeeded) {
+              updateStatus();
+          }
       }
       return map;
    }
-    
 }
