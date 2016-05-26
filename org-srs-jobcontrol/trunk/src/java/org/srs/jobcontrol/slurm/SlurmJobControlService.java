d947d490f08e833084d2b419de12909065007f8d
bvan <bvan@106b4de9-e96a-4e09-a5f9-d065fb1fdc55>
2016-05-26 22:35:00 +0000

    add slurm support

---
+++ org-srs-jobcontrol/trunk/src/java/org/srs/jobcontrol/slurm/SlurmJobControlService.java
@@ -0,0 +1,181 @@
+package org.srs.jobcontrol.slurm;
+
+import com.google.common.base.Supplier;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.RemoteServer;
+import java.rmi.server.ServerNotActiveException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+
+import javax.management.JMException;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import org.srs.jobcontrol.Job;
+import org.srs.jobcontrol.JobControl;
+import org.srs.jobcontrol.JobControlException;
+import org.srs.jobcontrol.JobStatus;
+import org.srs.jobcontrol.JobSubmissionException;
+import org.srs.jobcontrol.NoSuchJobException;
+import org.srs.jobcontrol.OutputProcessor;
+import org.srs.jobcontrol.common.CLIJobControlService;
+
+/**
+ * Slurm job submission daemon.
+ * @author bvan
+ */
+public class SlurmJobControlService extends CLIJobControlService {
+    private final String DEFAULT_SUBMIT = "sbatch";
+    private final String SUBMIT_COMMAND = 
+            System.getProperty("org.srs.jobcontrol.slurm.submitCommand", DEFAULT_SUBMIT);
+    private final static String KILL_COMMAND = 
+            System.getProperty("org.srs.jobcontrol.slurm.killCommand", "scancel");
+    
+    private final Supplier<Map<String, JobStatus>> statii;
+
+    /**
+     * 
+     * @param username Slurm user name
+     */
+    private SlurmJobControlService(String username){ 
+        statii = new SlurmStatusSupplier(username);
+    }
+    
+    public static void main(String[] args) throws RemoteException, JMException{
+        String user = System.getProperty("user.name");
+        SlurmJobControlService service = new SlurmJobControlService(user);
+        JobControl stub = (JobControl) UnicastRemoteObject.exportObject(service, 1097);
+        // Bind the remote object's stub in the registry
+        Registry registry = LocateRegistry.getRegistry();
+        registry.rebind("PBSJobControlService-" + user, stub);
+        SlurmJobControlService.logger.log(Level.INFO, "Server ready, user {0}", user);
+
+        // Register the JMX bean
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("org.srs.jobcontrol:type=JobControlService");
+        mbs.registerMBean(service, name);
+    }
+
+    @Override
+    public String submit(Job job) throws JobSubmissionException, JobControlException{
+        try {
+            logger.info("BEGIN slurm JobControlService submit");
+            String ip = RemoteServer.getClientHost();
+            logger.log(Level.INFO, "submitting: {0} from {1}", new Object[]{job.getCommand(), ip});
+            checkPermission(ip);
+
+            String jobName = submitInternal(job);
+            logger.log(Level.FINE, "job {0} submitted", jobName);
+            nSubmitted.incrementAndGet();
+            lastSuccessfulJobSubmissionTime = System.currentTimeMillis();
+            logger.info("END PBS JobControlService submit");
+            return jobName;
+        } catch(ServerNotActiveException ex) {
+            logger.log(Level.SEVERE, "Unexpected error", ex);
+            this.lastFailedJobSubmissionTime = System.currentTimeMillis();
+            throw new JobControlException("Unexpected error", ex);
+        } catch(JobSubmissionException | JobControlException ex) {
+            logger.log(Level.SEVERE, "job submission failed", ex);
+            this.lastFailedJobSubmissionTime = System.currentTimeMillis();
+            throw ex;
+        }
+    }
+
+    private String submitInternal(Job job) throws JobSubmissionException, JobControlException{
+        SlurmScriptJobBuilder jobBuilder = new SlurmScriptJobBuilder();
+        jobBuilder.build(job);
+        // submit
+        List<String> commands = new ArrayList<>();
+        commands.add(SUBMIT_COMMAND);
+        
+        String command = "pipeline_wrapper"; // normally would do job.getCommand(), but that won't work here
+        commands.add(command);
+        
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.redirectErrorStream(true);
+        builder.command(commands);
+        
+        Process process;
+        try {
+            process = builder.start();
+            OutputProcessor output = new OutputProcessor(process.getInputStream(), logger);
+            process.waitFor();
+            output.join();
+            int rc = process.exitValue();
+            if(rc != 0){
+                throw new JobControlException("Command failed rc=" + rc);
+            }
+            return extractJobId(output.getResult());
+        } catch(IOException | InterruptedException ex) {
+            logger.log(Level.SEVERE, "Unkown error submitting job", ex);
+            throw new JobControlException("Error submitting job", ex);
+        }
+    }
+
+    @Override
+    public void cancel(String jobID) throws NoSuchJobException, JobControlException{
+        try {
+            String ip = RemoteServer.getClientHost();
+            logger.log(Level.INFO, "killing: {0} from {1}", new Object[]{jobID, ip});
+            checkPermission(ip);
+            cancelInternal(jobID);
+            logger.log(Level.FINE, "job {0} cancelled", jobID);
+        } catch(ServerNotActiveException t) {
+            logger.log(Level.SEVERE, "Unexpected error", t);
+            throw new JobControlException("Unexpected error", t);
+        } catch(NoSuchJobException | JobControlException t) {
+            logger.log(Level.SEVERE, "job cancellation failed", t);
+            throw t;
+        }
+    }
+
+    private void cancelInternal(String jobID) throws NoSuchJobException, JobControlException{
+        try {
+            List<String> commands = new ArrayList<>();
+            commands.add(KILL_COMMAND);
+            commands.add(String.valueOf(jobID));
+            ProcessBuilder builder = new ProcessBuilder();
+            builder.redirectErrorStream(true);
+            builder.command(commands);
+            Process process = builder.start();
+            OutputProcessor output = new OutputProcessor(process.getInputStream(), logger);
+            process.waitFor();
+            output.join();
+            
+            int rc = process.exitValue();
+            if(rc == 255){
+                throw new NoSuchJobException("No such job, id=" + jobID);
+            } else if(rc != 0){
+                throw new JobControlException("Command failed rc=" + rc);
+            }
+        } catch(IOException x) {
+            throw new JobControlException("IOException while killing job " + jobID, x);
+        } catch(InterruptedException x) {
+            throw new JobControlException("InterruptedException while killing job " + jobID, x);
+        }
+    }
+
+    @Override
+    public Map<String, JobStatus> getCurrentStatus() throws JobControlException{
+        try{
+            return statii.get();
+        } catch (RuntimeException ex){
+            if(ex.getCause() instanceof JobControlException){
+                throw (JobControlException) ex.getCause();
+            }
+            throw new JobControlException("Unkown runtime exception occurred: ", ex.getCause());
+        }
+    }
+
+    @Override
+    public String extractJobId(List<String> result) throws JobSubmissionException{
+        return result.get(0).substring("Submitted batch job ".length());   // jobid at end of this string
+    }
+
+}
---
+++ org-srs-jobcontrol/trunk/src/java/org/srs/jobcontrol/slurm/SlurmScriptJobBuilder.java
@@ -0,0 +1,149 @@
+package org.srs.jobcontrol.slurm;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.srs.jobcontrol.Job;
+import org.srs.jobcontrol.JobSubmissionException;
+import org.srs.jobcontrol.common.JobControlService.DeleteFile;
+
+/**
+ * Slurm job builder.
+ * @author bvan
+ */
+public class SlurmScriptJobBuilder {
+    private static final String SHEBANG = "#!/bin/bash\n";
+    private final StringBuilder script;
+    private final List<Runnable> undoList;
+
+    SlurmScriptJobBuilder(){
+        script = new StringBuilder(SHEBANG);
+        undoList = new ArrayList<>();
+    }
+
+    public void build(Job job) throws JobSubmissionException{
+        try {
+            processWorkingDirectory(job);
+            processArchiveOldWorkingDir(job);
+            processName(job);
+            processLogFile(job);
+            processMaxCPU(job);
+            processMaxMemory(job);
+            processExtraOptions(job);
+            addSlurmDirective("-rn"); // Not re-runnable
+            processEnv(job);
+            processFiles(job);
+            script.append("\n");
+            script.append("bash pipeline_wrapper\n");
+        } catch (IOException ex){
+            for(Runnable undoItem: undoList){
+                undoItem.run();
+            }
+            throw new JobSubmissionException("Unable to build job", ex);
+        }
+    }
+
+    public String getPBSScript(){
+        return script.toString();
+    }
+
+    public void processArchiveOldWorkingDir(Job job) throws IOException{
+        if(job.getArchiveOldWorkingDir() != null){
+            doArchiveOldWorkingDir(job);
+        }
+    }
+
+    public void processEnv(Job job){
+        Map<String, String> env = job.getEnv();
+        for(String key: env.keySet()){
+            script.append(String.format("export %s=\"%s\"\n", key, env.get(key)));
+        }
+    }
+
+    public void processExtraOptions(Job job){
+        if(job.getExtraOptions() != null){
+            for(String s: tokenizeExtraOption(job.getExtraOptions())){
+                addSlurmDirective(s);
+            }
+        }
+    }
+
+    public void processFiles(Job job) throws IOException{
+        Map<String, String> files = job.getFiles();
+        Path basePath = Paths.get(job.getWorkingDirectory());
+        for(String name: files.keySet()){
+            Path target = basePath.resolve(name);
+            try (BufferedWriter writer = Files.newBufferedWriter(target, StandardOpenOption.CREATE_NEW)){
+                writer.write(files.get(name));
+                undoList.add(new DeleteFile(target.toFile()));
+            }
+        }
+    }
+
+    public void processLogFile(Job job){
+        String logFileName = job.getLogFile() == null ? "logFile.log" : sanitize(job.getLogFile());
+        addSlurmDirective(String.format("-o %s", logFileName));
+        job.getEnv().put("JOBCONTROL_LOGFILE", logFileName);
+    }
+
+    public void processMaxCPU(Job job){
+        if(job.getMaxCPU() != 0){
+            int minutes = job.getMaxCPU() / 60;
+            int seconds = job.getMaxCPU() % 60;
+            addSlurmDirective(String.format("-t %d:%d", minutes, seconds));
+        }
+    }
+
+    public void processMaxMemory(Job job){
+        if(job.getMaxMemory() != 0){
+            addSlurmDirective(String.format("--mem=%d", job.getMaxMemory()));
+        }
+    }
+
+    public void processName(Job job){
+        if(job.getName() != null){
+            addSlurmDirective(String.format("-J %s", sanitize(job.getName())));
+        }
+    }
+
+    public void processWorkingDirectory(Job job) throws IOException{
+        if(job.getWorkingDirectory() != null){
+            String dir = job.getWorkingDirectory();
+            Path dirPath = Paths.get(dir);
+            Files.createDirectories(dirPath);
+            addSlurmDirective(String.format("-D %s", job.getWorkingDirectory()));
+        }
+    }
+
+    private String sanitize(String option){
+        return option.replaceAll("\\s+", "_");
+    }
+    
+    private void doArchiveOldWorkingDir(Job job) throws IOException{
+        Path workingDir = Paths.get(job.getWorkingDirectory());
+        Path archiveDir = Paths.get(job.getArchiveOldWorkingDir());
+        Files.createDirectories(archiveDir);
+        for(Path file : Files.newDirectoryStream(workingDir)){
+            // Don't move archive root
+            if(!file.equals(archiveDir.getParent())){
+                Files.move(file, archiveDir.resolve(file.getFileName()));
+            }
+        }        
+    }
+
+    protected List<String> tokenizeExtraOption(String string){
+        return Arrays.asList(string.split("\\|"));
+    }
+
+    public void addSlurmDirective(String opt){
+        script.append(String.format("#SBATCH %s\n", opt));
+    }
+
+}
